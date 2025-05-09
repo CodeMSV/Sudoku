b961f58f01afd6e111074db96c933ae0d7d36a3f
savamidev <migue.626@gmail.com>
2025-05-09 09:21:56 +0200

    add removedCells field and constructor to DifficultyGame enum

--- src/main/java/com/sudoku/model/DifficultyGame.java
+++ src/main/java/com/sudoku/model/DifficultyGame.java
@@ -1,4 +1,23 @@
 package com.sudoku.model;
 
 public enum DifficultyGame {
+
+    EASY(35),
+    MEDIUM(45),
+    HARD(55);
+
+    private final int removedCells;
+
+    /**
+     * Constructor that initializes the difficulty level with a specified number of cells to remove.
+     *
+     * @param removedCells The number of cells to remove for this difficulty level.
+     */
+    DifficultyGame(int removedCells) {
+        this.removedCells = removedCells;
+    }
+
+    public int getRemovedCells() {
+        return removedCells;
+    }
 }
