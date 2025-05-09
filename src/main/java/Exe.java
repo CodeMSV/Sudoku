43630d15b01f86664a988fe214773f64e8fa119c
savamidev <migue.626@gmail.com>
2025-05-09 19:24:18 +0200

    add SudokuAppRunner to initialize and run the Sudoku application with controller and view setup

---
+++ src/main/java/Exe.java
@@ -0,0 +1,9 @@
+import com.sudoku.model.SudokuAppRunner;
+
+public class Exe {
+
+    public static void main(String[] args) {
+        SudokuAppRunner sudokuAppRunner = new SudokuAppRunner();
+        sudokuAppRunner.ejecutar();
+    }
+}
