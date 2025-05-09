75aa40d5bfbe47921050c507e0fa5b95db64dffe
savamidev <migue.626@gmail.com>
2025-05-09 19:23:41 +0200

    refactor SudokuGeneratorBackTrackingImp for code clarity and style improvements; add initial test class for SudokuGenerator

--- src/main/java/com/sudoku/model/generatorGame/SudokuGeneratorBackTrakingImp.java
+++ src/main/java/com/sudoku/model/generatorGame/SudokuGeneratorBackTrakingImp.java
@@ -2,14 +2,12 @@ package com.sudoku.model.generatorGame;
 
 import com.sudoku.model.DifficultyGame;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SudokuGeneratorBackTrakingImp implements SudokuGenerator {
-
 
     /**
      * Generates a Sudoku puzzle based on the specified difficulty level.
@@ -24,7 +22,6 @@ public class SudokuGeneratorBackTrakingImp implements SudokuGenerator {
         return boardGame;
     }
 
-
     /**
      * Fills the Sudoku grid with numbers from 1 to 9 using backtracking.
      *
@@ -35,35 +32,35 @@ public class SudokuGeneratorBackTrakingImp implements SudokuGenerator {
         for (int row = 0; row < 9; row++) {
 
             for (int col = 0; col < 9; col++) {
-
                 if (boardGame[row][col] == 0) {
-                    List<Integer> numbers = IntStream.rangeClosed(1, 9).boxed().collect(Collectors.toList());
+                    List<Integer> numbers = IntStream.rangeClosed(1, 9)
+                            .boxed()
+                            .collect(Collectors.toList());
                     Collections.shuffle(numbers);
 
                     for (int num : numbers) {
                         if (isValid(boardGame, row, col, num)) {
                             boardGame[row][col] = num;
-
                             if (fillBoardGame(boardGame)) return true;
                             boardGame[row][col] = 0;
                         }
                     }
-                }
 
+                    return false;
+                }
             }
         }
 
         return true;
     }
 
-
     /**
      * Valide/Check if a number can be placed in a cell of de Sudoku grid board.
      *
      * @param boardGame The Sudoku grid.
-     * @param row The row index.
-     * @param col The column index.
-     * @param num The number to check.
+     * @param row       The row index.
+     * @param col       The column index.
+     * @param num       The number to check.
      * @return true if the number can be placed, false otherwise.
      */
     private boolean isValid(int[][] boardGame, int row, int col, int num) {
@@ -76,9 +73,10 @@ public class SudokuGeneratorBackTrakingImp implements SudokuGenerator {
         int blockStartRowIndex = (row / 3) * 3;
         int blockStartColIndex = (col / 3) * 3;
 
-        for (int checkRowIndex = blockStartRowIndex; checkRowIndex < (blockStartRowIndex + 3); checkRowIndex++) {
-            for (int checkColIndex = blockStartColIndex; checkColIndex < (blockStartColIndex + 3); checkColIndex++) {
-                if (boardGame[checkRowIndex][checkColIndex] == num) {
+        for (int r = blockStartRowIndex; r < blockStartRowIndex + 3; r++) {
+
+            for (int c = blockStartColIndex; c < blockStartColIndex + 3; c++) {
+                if (boardGame[r][c] == num) {
                     return false;
                 }
             }
@@ -87,25 +85,3 @@ public class SudokuGeneratorBackTrakingImp implements SudokuGenerator {
         return true;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
