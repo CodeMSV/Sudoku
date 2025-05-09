b5fbe00f355bd8e63556904df681e2f1bef51ef3
savamidev <migue.626@gmail.com>
2025-05-09 12:31:37 +0200

    implement new game functionality and hint system in SudokuController

--- src/main/java/com/sudoku/controller/SudokuController.java
+++ src/main/java/com/sudoku/controller/SudokuController.java
@@ -1,8 +1,12 @@
 package com.sudoku.controller;
 
+import com.sudoku.model.DifficultyGame;
 import com.sudoku.model.SudokuBoard;
 import com.sudoku.model.generatorGame.SudokuGenerator;
 import com.sudoku.view.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class SudokuController {
 
@@ -20,7 +24,50 @@ public class SudokuController {
     }
 
     private void initController() {
-
+       // view.newGameBtn.addActionListener(e -> newGame());
     }
+
+    private void  newGame() {
+        DifficultyGame difficultyGame = (DifficultyGame) view.difficultyCombo.getSelectedItem();
+        int[][] solutionGame = generator.generate(difficultyGame);
+        setHintsByDifficulty(difficultyGame);
+        //updateBoard();
+    }
+
+    private void setHintsByDifficulty(DifficultyGame difficultyGame) {
+        switch (difficultyGame) {
+            case EASY -> hintsRemaining = 5;
+            case MEDIUM -> hintsRemaining = 3;
+            case HARD -> hintsRemaining = 2;
+            default -> hintsRemaining = 3;
+        }
+
+        view.hintBtn.setText("💡 Hint (" + this.hintsRemaining + ")");
+        view.hintBtn.setEnabled(true);
+    }
+
+    private void updateBoard() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                JTextField cell = view.cells[row][col];
+
+                int value = model.getCurrentValue(row, col);
+
+                if (value != 0) {
+                    cell.setText(String.valueOf(value));
+                    cell.setEditable(false);
+                    cell.setForeground(Color.BLUE);
+                } else {
+                    cell.setText("");
+                    cell.setEditable(true);
+                    cell.setForeground(Color.BLACK);
+                }
+
+            }
+
+        }
+    }
+
+
 
 }
